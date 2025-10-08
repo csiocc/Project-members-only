de2170420e45d36fa2f15897c9d23bdc4dfd8a33
Christian <christian.siegrist@open-circle.ch>
2025-10-08 12:48:01 +0200

    fix: post deletion with replys on comments works now

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,13 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :post
+  belongs_to :post, inverse_of: :comments
   belongs_to :parent, class_name: "Comment", optional: true, inverse_of: :replies
 
   has_many :replies, -> { order(created_at: :asc) },
-           class_name: "Comment", foreign_key: :parent_id, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  has_many :root_comments, -> { where(parent_id: nil).order(:created_at) }, class_name: "Comment"
-
+           class_name: "Comment", 
+           foreign_key: :parent_id, 
+           dependent: :destroy,
+           inverse_of: :parent
 
   scope :roots, -> { where(parent_id: nil) }
 
